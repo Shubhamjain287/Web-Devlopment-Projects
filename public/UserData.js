a100190f3e38050218e636cdf73737ffd7685493
Divyesh1511 <khuvadivyesh15@gmail.com>
2022-10-14 09:27:45 +0530

    Added name in userData and corrected folder name

--- public/UserData.js
+++ public/UserData.js
@@ -6,6 +6,13 @@ const UserData = [
       imgsrc : "https://1.bp.blogspot.com/-23yVlT_sCP4/YLS4fGUfbPI/AAAAAAAAHvo/TkZ2xSnXTHs2XRlRa3q4do5tgfvjozaSwCLcBGAsYHQ/s320/PicsArt_05-31-03.50.1",
       github : "https://github.com/Shubhamjain287"
    }
+   {
+      id : 2,
+      Name : "Divyesh Khuva",
+      Description : "Open Source Contribution",
+      imgsrc : "https://avatars.githubusercontent.com/u/75933218?s=400&v=4",
+      github : "https://github.com/Divyesh1511"
+   }
 ];
 
 export default UserData;
