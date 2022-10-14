70fa69e5acf54f56d7cee3a612aaee5100dfbbf6
vinit solanki <vinitsolanki0987@gmail.com>
2022-10-14 10:24:25 +0530

    Uploding My Contact Management Project

--- public/UserData.js
+++ public/UserData.js
@@ -5,6 +5,12 @@ const UserData = [
       Description : "Open Source Contribution",
       imgsrc : "https://1.bp.blogspot.com/-23yVlT_sCP4/YLS4fGUfbPI/AAAAAAAAHvo/TkZ2xSnXTHs2XRlRa3q4do5tgfvjozaSwCLcBGAsYHQ/s320/PicsArt_05-31-03.50.1",
       github : "https://github.com/Shubhamjain287"
+   },{
+      id : 2,
+      Name : "Vinit Solanki",
+      Description : "Coding Enthusiaster",
+      imgsrc : "https://avatars.githubusercontent.com/u/99660135?s=400&u=d55814ed14f6ba0c22374270bb860df3b01fe610&v=4",
+      github : "https://github.com/vinit2926"
    }
 ];
 
